ef2be8821d06ea219cb6656fa1f0410cd60d3e88
JimKTran <71141264+JimKTran@users.noreply.github.com>
2020-11-17 12:34:19 -0500

    Update Tables_Schemata.sql

--- EmployeeSQL/Tables_Schemata.sql
+++ EmployeeSQL/Tables_Schemata.sql
@@ -2,6 +2,12 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/ctGtj2
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+--DROP TABLE departments
+--DROP TABLE dept_emp
+--DROP TABLE dept_manager
+-- DROP TABLE employees
+-- DROP TABLE salaries
+-- DROP TABLE titles
 
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
@@ -23,17 +29,26 @@ CREATE TABLE "titles" (
 
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL
+    "dept_no" varchar   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "departments" (
@@ -41,18 +56,18 @@ CREATE TABLE "departments" (
     "dept_name" varchar   NOT NULL
 );
 
---ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
---REFERENCES "dept_emp" ("emp_no");
+-- ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
+-- REFERENCES "dept_emp" ("emp_no");
 
---ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
---REFERENCES "employees" ("emp_title_id");
+-- ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
+-- REFERENCES "employees" ("emp_title_id");
 
---ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
---REFERENCES "salaries" ("emp_no");
+-- ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+-- REFERENCES "salaries" ("emp_no");
 
---ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
---REFERENCES "departments" ("dept_no");
+-- ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+-- REFERENCES "departments" ("dept_no");
 
---ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
---REFERENCES "dept_manager" ("dept_no");
+-- ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+-- REFERENCES "dept_manager" ("dept_no");
 
