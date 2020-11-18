6612decf13b21334be3375e5566e40d92d37fe7a
JimKTran <71141264+JimKTran@users.noreply.github.com>
2020-11-18 14:49:46 -0500

    updates

--- EmployeeSQL/Tables_Schemata.sql
+++ EmployeeSQL/Tables_Schemata.sql
@@ -16,36 +16,19 @@ CREATE TABLE "employees" (
      )
 );
 
-CREATE TABLE "titles" (
-    "title_id" varchar   NOT NULL,
-    "title" varchar   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
-);
-
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "dept_manager" (
-    "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no"
-     )
+    "salary" int   NOT NULL
 );
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
     "dept_no" varchar   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no"
-     )
+    "emp_no" int   NOT NULL
 );
 
 CREATE TABLE "departments" (
@@ -56,18 +39,26 @@ CREATE TABLE "departments" (
      )
 );
 
+CREATE TABLE "titles" (
+    "title_id" varchar   NOT NULL,
+    "title" varchar   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "dept_manager" ("dept_no");
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
