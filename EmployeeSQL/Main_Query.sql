ba77e8cb3daa85a4d46f0acda5d0baf7cffe169c
JimKTran <71141264+JimKTran@users.noreply.github.com>
2020-11-19 11:14:37 -0500

    Update Main_Query.sql

--- EmployeeSQL/Main_Query.sql
+++ EmployeeSQL/Main_Query.sql
@@ -1,3 +1,5 @@
+-- Data Analysis for SQL homework
+
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
 SELECT e.emp_no as "Employee Number"
@@ -19,6 +21,58 @@ WHERE e.hire_date between '01/01/1986' and '12/31/1986'
 ORDER BY e.hire_date DESC;
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+SELECT dm.dept_no as "Department Number", d.dept_name as "Department Name", e.emp_no as "Employee Number", e.last_name as "Last Name", e.first_name as "First Name", ti.title as "Title"
+FROM employees e
+	JOIN dept_manager dm
+	ON e.emp_no = dm.emp_no
+		JOIN departments d
+		ON dm.dept_no = d.dept_no
+			JOIN titles ti
+			ON e.emp_title_id = ti.title_id
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
